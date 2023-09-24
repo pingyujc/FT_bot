d755c52b8d05eeab4107e86eab82cfcb1fabadac
pingyujc <pingyujc@gmail.com>
2023-09-24 13:20:38 +0800

    init

---
+++ bot.py
@@ -0,0 +1,21 @@
+# import the needed libraries
+import os
+from dotenv import dotenv_values
+
+# APIs, hide later
+# BASESCAN_API_KEY = "I62GI6JFWIX6EGH7WTVJR6CVSR3JV4GHPE"
+
+# main algo here
+def main():
+
+    params = {
+
+    }
+    print("This is a FriendTech Arbitrage bot")
+
+
+
+
+# run it
+if __name__ == "__main__":
+    main()
