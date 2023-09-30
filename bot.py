5cfd2a7e427b4443b47c312c73dd22684f5d385b
pingyujc <pingyujc@gmail.com>
2023-09-29 23:41:07 -0700

    can scan through latest block and filter

--- bot.py
+++ bot.py
@@ -3,6 +3,9 @@ import os
 from dotenv import load_dotenv
 import requests
 from web3 import Web3
+import time
+import telegram
+from telegram.ext import Updater, CommandHandler
 
 
 
@@ -12,22 +15,9 @@ load_dotenv()
 BASESCAN_API_KEY = os.getenv("BASESCAN_API_KEY")
 BASESCAN_URL = os.getenv("BASESCAN_URL")
 FT_ADDRESS = os.getenv("FT_ADDRESS")
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 
-params = {
-    "address": FT_ADDRESS,
-    "apikey": BASESCAN_API_KEY,  # Include your API key here if required
-}
 
-'''
-
-https://api.basescan.org/api
-   ?module=account
-   &action=balance
-   &address=0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae
-   &tag=latest
-   &apikey=YourApiKeyToken
-
-'''
 # this function will generate the API URL call based on our need
 def make_api_url(module, action, address, tag, apikey):
     url = "https://api.basescan.org/api"
@@ -35,10 +25,10 @@ def make_api_url(module, action, address, tag, apikey):
     return url
 
 # try getting the balance
-input_address = input("please enter address: ")
-# get_balance_url = make_api_url("account", "balance", FT_ADDRESS, "latest", BASESCAN_API_KEY)
+# input_address = input("please enter address: ")
+get_balance_url = make_api_url("account", "balance", FT_ADDRESS, "latest", BASESCAN_API_KEY)
 
-get_balance_url = make_api_url("account", "balance", input_address, "latest", BASESCAN_API_KEY)
+# get_balance_url = make_api_url("account", "balance", input_address, "latest", BASESCAN_API_KEY)
 
 
 def run_api(url):
@@ -64,12 +54,61 @@ if not w3.is_connected():
 else:
     print("Connected to Base network")
 
-# get the latest block number
-block_number = w3.eth.block_number
-print(block_number)
+def testing_one_block(block_num):
+    block_number = block_num
+    block = w3.eth.get_block('latest')
+    print("BLOCK NUMBER:", block_number)
+    for tx_hash in block['transactions']:
+        # get the transaction
+        tx = w3.eth.get_transaction(tx_hash)
+        # print(f"tx: {tx} \n")
 
-# get the latest block info
-block = w3.eth.get_block('latest')
-print(block)
+        # decode the tx data
+        input_data = tx['input']
+        # get the method ID to filter
+        # transformed to hex, and get the first 10 letters only
+        method_ID = input_data[:10].hex()[:10]
+
+        if method_ID == buying_share:
+            print("buying share...")
+        elif method_ID == selling_share:
+            print("selling share...")
+
+        print(f"method ID: {method_ID}")
+
+
+# the method ID for buying and selling shares on FT:
+buying_share = '0x6945b123'
+selling_share = '0xb51d0534'
+
+# time interval between loop
+interval = 1
+
+# while loop here to make sure it keeps running
+while True:
+    # get the latest block number
+    block_number = w3.eth.block_number
+    block = w3.eth.get_block('latest')
+    print("BLOCK NUMBER:", block_number)
+    for tx_hash in block['transactions']:
+        # get the transaction
+        tx = w3.eth.get_transaction(tx_hash)
+        # print(f"tx: {tx} \n")
+
+        # decode the tx data
+        input_data = tx['input']
+        # get the method ID to filter
+        # transformed to hex, and get the first 10 letters only
+        method_ID = input_data[:10].hex()[:10]
+
+        if method_ID == buying_share:
+            print("buying share...")
+        elif method_ID == selling_share:
+            print("selling share...")
+
+        print(f"method ID: {method_ID}")
+    print("\n")
+    time.sleep(interval)
+    
 
 
